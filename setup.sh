6e7ce0c98484ab7d3f2014d4061d6fdf286b6c63
AmitSarkar2051 <54927343+AmitSarkar2051@users.noreply.github.com>
2024-12-17 03:10:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ rm -rf ~/miniforge.sh
 
 # Install packages
 echo "SETUP.SH: Starting INstallation"
-${PYTHON_PREFIX}/bin/pip install -r requirements.txt
+${PYTHON_PREFIX}/bin/pip install -r requirement.txt
 echo "SETUP.SH: streamlit installation DONE!!!"
 
 # Run app
