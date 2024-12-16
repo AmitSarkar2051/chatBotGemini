7fed73229fa1d26b5765bd922b78f2c73bb16b1f
AmitSarkar2051 <54927343+AmitSarkar2051@users.noreply.github.com>
2024-12-17 02:45:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,9 @@ bash ~/miniforge.sh -fbp ${PYTHON_PREFIX}
 rm -rf ~/miniforge.sh
 
 # Install packages
+echo "SETUP.SH: Starting INstallation"
 ${PYTHON_PREFIX}/bin/pip install -r requirements.txt
+echo "SETUP.SH: streamlit installation DONE!!!"
 
 # Run app
 ${PYTHON_PREFIX}/bin/streamlit run app.py --server.enableCORS=false --server.enableXsrfProtection=false --server.port 8080
